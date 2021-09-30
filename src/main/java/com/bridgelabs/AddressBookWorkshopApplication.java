a8b7a2ad1b58644ad75e626bfc216a58cde0081d
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-09-30 13:29:50 +0530

    [Pritesh] Initial Setup

---
+++ src/main/java/com/bridgelabs/AddressBookWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookWorkshopApplication.class, args);
+	}
+
+}
